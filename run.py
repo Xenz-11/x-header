b4d5e007447b97c9f664d3c725b969970f9bb7b2
MR. XENZ <102963507+Xenz-11@users.noreply.github.com>
2022-06-08 23:30:30 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+try:
+    import os,sys,requests
+except ImportError:
+    os.system('pip install requests')
+    os.system('pip install mechanize')
+    os.system('git pull')
+os.system('python x-header.cython-easy-38.so')
